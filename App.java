dbea5624584d17d0765009be835c9d6b0a7ed1f6
glarinengufy-design <glarinengufy@gmail.com>
2026-05-16 00:36:53 +0800

    Update App.java

--- App.java
+++ App.java
@@ -5,7 +5,34 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception { 
-        Repository repo = new Repository();
-        
+        Scanner scanner = new Scanner(System.in);
+        List<Product> products = new ArrayList<>();
+        List<User> users = new ArrayList<>();
+        List<Order> orders = new ArrayList<>();
+        Repository repo = new Repository(products, orders, users);
+
+        Admin admin = new Admin(1, "AdminUser");
+        Customer customer = new Customer(2, "CustUser");
+
+        repo.addUser(admin);
+        repo.addUser(customer);
+
+        Product carrot = new Vegetable("V001", "Carrot", "Vegetable", 1.5, 50, true, "2026-05-30");
+        // repo.addProduct(carrot);
+
+        System.out.println("Welcome to ");
+        System.out.println("Did you have any account?(yes/no)");
+        String choose = scanner.nextLine();
+        if (choose=="yes") {
+            System.out.print("Please enter your username: ");
+            String inputUsername = scanner.nextLine();
+            System.out.print("Please enter your password: ");
+            String inputPAssword = scanner.nextLine();
+            User.login(repo, inputUsername, inputPAssword);
+        } else if (choose=="no") {
+            User.RegisterAccount(repo);
+        } else {
+            
+        }
     }
 }
