3a5d1a6aa2274095d82c77ba33348c71a7292e05
glarinengufy-design <glarinengufy@gmail.com>
2026-05-16 00:37:06 +0800

    Update Snack.java

--- Snack.java
+++ Snack.java
@@ -1,7 +1,8 @@
-public class Snack {
+public class Snack extends Product {
     private String brand;
 
-    public Snack(String brand){
+    public Snack(String productId, String productName, String category, double price, int quantity, boolean available, String brand){
+        super(productId,productName, category, price, quantity, available);
         this.brand = brand;
     }
 
