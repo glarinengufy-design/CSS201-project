1f273905ee95f65b261ddecd512f7e80c6561e2a
glarinengufy-design <glarinengufy@gmail.com>
2026-05-15 23:56:45 +0800

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,11 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception { 
-    
+        Repository repo = new Repository();
+        
     }
 }
