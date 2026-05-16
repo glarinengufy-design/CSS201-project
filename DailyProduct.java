282e9c6c91c79af0c444c75b299fbb5bd41023c5
glarinengufy-design <glarinengufy@gmail.com>
2026-05-16 10:11:33 +0800

    Update DailyProduct.java

--- DailyProduct.java
+++ DailyProduct.java
@@ -1,7 +1,8 @@
-public class DailyProduct {
+public class DailyProduct extends Product{
     private String brand;
 
-    public DailyProduct(String brand){
+    public DailyProduct(String productId, String productName, String category, double price, int quantity, boolean available, String brand){
+        super(productId,productName, category, price, quantity, available);
         this.brand = brand;
     }
 
