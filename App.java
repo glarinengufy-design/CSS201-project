e77aa66d0f01d79f17fc85662a137e9e23deea41
glarinengufy-design <glarinengufy@gmail.com>
2026-05-16 22:12:58 +0800

    Update App.java

--- App.java
+++ App.java
@@ -11,28 +11,31 @@ public class App {
         List<Order> orders = new ArrayList<>();
         Repository repo = new Repository(products, orders, users);
 
-        Admin admin = new Admin(64645,"fafa123","dnfdj","fafa445",014333555,"Fafa","Admin");
-        Customer customer = new Customer(64645,"fafa123","dnfdj","fafa445",014333555,"Fafa","Customer");
+        Admin admin = new Admin(64645,"fafa123","dnfdj","fafa445","014-333555","Fafa","Admin");
+        Customer customer = new Customer(64645,"fafa123","dnfdj","fafa445","014-333555","Fafa","Customer", 64645, "Fafa");
 
         repo.addUser(admin);
         repo.addUser(customer);
 
         Product carrot = new Vegetable("V001", "Carrot", "Vegetable", 1.5, 50, true, "2026-05-30");
-        // repo.addProduct(carrot);
+        repo.addProduct(carrot);
 
         System.out.println("Welcome to ");
-        System.out.println("Did you have any account?(yes/no)");
+        System.out.println("Did you have any account? (yes/no)");
         String choose = scanner.nextLine();
-        if (choose=="yes") {
+        if (choose.equalsIgnoreCase("yes")) {
             System.out.print("Please enter your username: ");
             String inputUsername = scanner.nextLine();
             System.out.print("Please enter your password: ");
-            String inputPAssword = scanner.nextLine();
-            User.login(repo, inputUsername, inputPAssword);
-        } else if (choose=="no") {
-            User.registerAccount(repo);
+            String inputPassword = scanner.nextLine();
+            User.login(inputUsername, inputPassword);
+        } else if (choose.equalsIgnoreCase("no")) {
+            User newUser = User.registerAccount();
+            if (newUser != null) {
+                repo.addUser(newUser);
+            }
         } else {
-            
+            System.out.println("Invalid selection. Please restart the program and enter yes or no.");
         }
     }
 }
