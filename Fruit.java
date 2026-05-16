55f4c21c35b3049167ddf536ff4ae0fbfd20796c
glarinengufy-design <glarinengufy@gmail.com>
2026-05-16 10:11:37 +0800

    Update Fruit.java

--- Fruit.java
+++ Fruit.java
@@ -1,7 +1,8 @@
-public class Fruit {
+public class Fruit extends Product{
     private String expiryDate;
 
-    public Fruit(String expiryDate){
+    public Fruit(String productId, String productName, String category, double price, int quantity, boolean available, String expiryDate){
+        super(productId,productName, category, price, quantity, available);
         this.expiryDate = expiryDate;
     }
 
