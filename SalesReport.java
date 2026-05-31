681c452d146ebf29aeaa65f62d94bee2cf5db884
glarinengufy-design <glarinengufy@gmail.com>
2026-05-31 22:38:52 +0800

    Update SalesReport.java

--- SalesReport.java
+++ SalesReport.java
@@ -21,17 +21,72 @@ public class SalesReport {
         return salesMap;
     }
 
-    // public Product getTopSalesProduct() {
-    //     Map<String, Integer> salesMap = buildSalesMap();
-    //     String topId = null;
-    //     int max = 0;
+    public Product getTopSalesProduct() {
+        Map<String, Integer> salesMap = buildSalesMap();
+        String topId = null;
+        int max = 0;
 
-    //     for (Map.Entry<String, Integer> entry : salesMap.entrySet()) {
-    //         if (entry.getValue() > max) {
-    //             max = entry.getValue();
-    //             topId = entry.getKey();
-    //         }
-    //     }
-    //     return topId != null ? repository.viewProductById(topId) : null;
-    // }
+        for (Map.Entry<String, Integer> entry : salesMap.entrySet()) {
+            if (entry.getValue() > max) {
+                max = entry.getValue();
+                topId = entry.getKey();
+            }
+        }
+        return topId != null ? repository.viewProductById(topId) : null;
+    }
+
+    // Least-selling product
+    public Product getLeastSalesProduct() {
+        Map<String, Integer> salesMap = buildSalesMap();
+        String leastId = null;
+        int min = Integer.MAX_VALUE;
+
+        for (Map.Entry<String, Integer> entry : salesMap.entrySet()) {
+            if (entry.getValue() < min) {
+                min = entry.getValue();
+                leastId = entry.getKey();
+            }
+        }
+        return leastId != null ? repository.viewProductById(leastId) : null;
+    }
+
+    // Total number of orders
+    public int getTotalOrders() {
+        return repository.getOrders().size();
+    }
+
+    // Total earnings (sum of all order totals)
+    public double getTotalEarnings() {
+        double total = 0;
+        for (Order order : repository.getOrders()) {
+            total += order.getTotalCost();
+        }
+        return total;
+    }
+
+    // Average earnings per order
+    public double getAverageEarnings() {
+        int totalOrders = getTotalOrders();
+        return totalOrders > 0 ? getTotalEarnings() / totalOrders : 0;
+    }
+
+    // Display full sales report
+    public void displaySalesReport() {
+        System.out.println("\n=== Sales Report ===");
+        System.out.println("Total Orders: " + getTotalOrders());
+        System.out.println("Total Earnings: RM " + getTotalEarnings());
+        System.out.println("Average Earnings per Order: RM " + getAverageEarnings());
+
+        Product topProduct = getTopSalesProduct();
+        Product leastProduct = getLeastSalesProduct();
+
+        if (topProduct != null) {
+            System.out.println("Top-Selling Product: " + topProduct.getProductName());
+        }
+        if (leastProduct != null) {
+            System.out.println("Least-Selling Product: " + leastProduct.getProductName());
+        }
+
+        System.out.println("=====================");
+    }
 }
